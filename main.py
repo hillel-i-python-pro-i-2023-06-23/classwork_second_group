e97eb28c8d19011597a1f75261b58241154772b0
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2023-06-28 14:17:20 +0300

    Добавил возможность нескольких игроков

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import random
 # Задачи:
 # ✅Добавить менюшку с именем (точнее просто перенести ее с куска Юры)
-# Добавить возможность игры еще с одним или больше людьми как реализовать пока хз
+# ✅Добавить возможность игры еще с одним или больше людьми как реализовать пока хз
 
-def play_game():
-    # Списки слов для выбора
+import random
+
+
+def add_players():
+    players = []
+    num_players = int(input("Введите количество участников: "))
+    for i in range(num_players):
+        name = input(f"Введите имя участника {i+1}: ")
+        players.append(name)
+    return players
+
+
+def play_game(players):
     word_lists = [
         {
             "name": "Фрукты",
@@ -15,63 +26,62 @@ def play_game():
             "words": ["морковь", "помидор", "огурец", "капуста", "брокколи", "редиска"]
         }
     ]
-    # Ввод имени
-    player_registration = input('Впиши свое имя: ')
-    print(f'Привет {player_registration.capitalize()}')
 
-    # Выбор списка слов пользователем
-    print("Выберите категорию слов:")
-    for i, word_list in enumerate(word_lists):
-        print(f"{i + 1}. {word_list['name']}")
-    choice = int(input("Введите номер категории: ")) - 1
+    for player in players:
+        print(f"\nИгра для участника {player}:")
+        print("------------------------")
 
-    # Проверка валидности выбора категории
-    if choice < 0 or choice >= len(word_lists):
-        print("Неверный выбор категории. Игра завершена.")
-        return
+        print("Выберите категорию слов:")
+        for i, word_list in enumerate(word_lists):
+            print(f"{i + 1}. {word_list['name']}")
+        choice = int(input("Введите номер категории: ")) - 1
 
-    # Выбор случайного слова из выбранной категории
-    word_list = word_lists[choice]
-    word = random.choice(word_list["words"])
-    guessed_letters = []
-    attempts = 6
+        if choice < 0 or choice >= len(word_lists):
+            print("Неверный выбор категории. Игра завершена.")
+            continue
 
-    while attempts > 0:
-        hidden_word = ""
-        for letter in word:
-            if letter in guessed_letters:
-                hidden_word += letter
+        word_list = word_lists[choice]
+        word = random.choice(word_list["words"])
+        guessed_letters = []
+        attempts = 6
+
+        while attempts > 0:
+            hidden_word = ""
+            for letter in word:
+                if letter in guessed_letters:
+                    hidden_word += letter
+                else:
+                    hidden_word += "_"
+
+            print("Скрытое слово:", hidden_word)
+            print("Попыток осталось:", attempts)
+
+            if "_" not in hidden_word:
+                print("Поздравляю! Вы угадали слово!")
+                break
+
+            guess = input("Угадайте букву или введите слово целиком: ").lower()
+
+            if guess == word:
+                print("Поздравляю! Вы угадали слово!")
+                break
+
+            if len(guess) == 1:
+                if guess in guessed_letters:
+                    print("Вы уже угадывали эту букву. Попробуйте еще раз!")
+                elif guess in word:
+                    print("Отлично! Буква есть в слове.")
+                    guessed_letters.append(guess)
+                else:
+                    print("Упс! Эта буква отсутствует в слове.")
+                    attempts -= 1
+                    guessed_letters.append(guess)
             else:
-                hidden_word += "_"
+                print("Упс! Неверное слово. Попробуйте еще раз!")
 
-        print("Скрытое слово:", hidden_word)
-        print("Попыток осталось:", attempts)
-
-        if "_" not in hidden_word:
-            print("Поздравляю! Вы угадали слово!")
-            break
-
-        guess = input("Угадайте букву или введите слово целиком: ").lower()
-
-        if guess == word:
-            print("Поздравляю! Вы угадали слово!")
-            break
-
-        if len(guess) == 1:
-            if guess in guessed_letters:
-                print("Вы уже угадывали эту букву. Попробуйте еще раз!")
-            elif guess in word:
-                print("Отлично! Буква есть в слове.")
-                guessed_letters.append(guess)
-            else:
-                print("Упс! Эта буква отсутствует в слове.")
-                attempts -= 1
-                guessed_letters.append(guess)
-        else:
-            print("Упс! Неверное слово. Попробуйте еще раз!")
-
-    if attempts == 0:
-        print("Игра окончена! У вас закончились попытки. Слово было:", word)
+        if attempts == 0:
+            print("Игра окончена! У вас закончились попытки. Слово было:", word)
 
 
-play_game()
+players = add_players()
+play_game(players)
